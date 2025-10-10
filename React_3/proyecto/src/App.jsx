38f9dd5c5b69613e7381cf9a486303199d72e7e5
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-10 10:57:03 +0200

    react

--- React_3/proyecto/src/App.jsx
+++ React_3/proyecto/src/App.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-
-import Index from "./pages/Index";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<Index />} />
+      <Route path="/" element={<Home />} />
       <Route path="/proyectos" element={<div style={{paddingTop: '100px', padding: '100px 2rem'}}>Página de Proyectos (En construcción)</div>} />
       <Route path="/sobre-mi" element={<div style={{paddingTop: '100px', padding: '100px 2rem'}}>Página Sobre Mí (En construcción)</div>} />
     </Routes>
