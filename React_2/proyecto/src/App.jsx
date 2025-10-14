a6768e732c7da89f08c74f3a99df19ae22cb4880
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-14 23:05:27 +0200

    Update App.jsx

--- React_2/proyecto/src/App.jsx
+++ React_2/proyecto/src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
+import Home from "./pages/Home";
+import Home_copy from "./pages/Home_copy";
+
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Index />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/Home_copy" element={<Home_copy />} />
+      <Route path="/pages/Index" element={<Index />} />
+
       <Route path="/proyectos" element={<div style={{paddingTop: '100px', padding: '100px 2rem'}}>Página de Proyectos (En construcción)</div>} />
       <Route path="/sobre-mi" element={<div style={{paddingTop: '100px', padding: '100px 2rem'}}>Página Sobre Mí (En construcción)</div>} />
     </Routes>
