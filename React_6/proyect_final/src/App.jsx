5701fc285c9696c8422045d66316cbc9385651b9
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-17 11:14:20 +0200

    Update App.jsx

--- React_6/proyect_final/src/App.jsx
+++ React_6/proyect_final/src/App.jsx
@@ -4,18 +4,20 @@ import Home from './pages/Home'
 import './App.css'
 import Cabecera from "./components/Cabecera";
 import Header from "./components/Header";
-import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+// import NavBar from "./components/NavBar";
 
 export default function App() {
   return (   
     <>
     <Header />
     
-    <Cabecera />
+    {/* <Cabecera /> */}
     
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
+    <Footer />
     </>
   );
 }
