166c66e9cce002a233c0596b2e59e1be43d9da3c
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-28 10:25:20 +0100

    Create servidor.js

---
+++ EXPRESS/servidor.js
@@ -0,0 +1,14 @@
+// servidor.js
+import express from 'express';
+
+const app= express();
+const PORT= 3000;
+
+app.get('/', (req,res) => {
+    res.send("🎉 ¡Hola mundo desde Express!");
+});
+
+app.listen(PORT, () => {
+    console.log("✅ Servidor escuchando en http://localhost:3000")
+})
+
