96331ea37f581a5e6d4da2c7b8d66a9b5d5cd0ef
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-29 11:59:17 +0100

    expres

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -3,25 +3,25 @@ const boton1 = document.getElementById("boton1");
 const boton2 = document.getElementById("boton2"); 
 const boton3 = document.getElementById("boton3"); 
 const boton4 = document.getElementById("boton4"); 
-const resultado = document.getElementById("resultado");
+const resultados = document.getElementById("resultados");
 
 boton1.addEventListener("click", () => {
     fetch("http://localhost:3000/")
     .then((res) => res.text()) // texto JSON → objeto JS 
-    .then((data) => { resultado.textContent = data;}) 
+    .then((data) => { resultados.textContent = data;}) 
     }); 
-// boton2.addEventListener("click", () => {
-//     fetch("http://localhost:3000/contacto")
-//     .then((res) => res.json()) // texto JSON → objeto JS 
-//     .then((data) => { resultado.textContent = data.nombre + " — " + data.curso;}) 
-//     }); 
-// boton3.addEventListener("click", () => {
-//     fetch("http://localhost:3000/info")
-//     .then((res) => res.json()) // texto JSON → objeto JS 
-//     .then((data) => { resultado.textContent = data.mensaje + " — " + data.curso;}) 
-//     }); 
-// boton4.addEventListener("click", () => {
-//     fetch("http://localhost:3000/tienda")
-//     .then((res) => res.json()) // texto JSON → objeto JS 
-//     .then((data) => { resultado.textContent = data.mensaje + " — " + data.curso;}) 
-//     }); 
+boton2.addEventListener("click", () => {
+    fetch("http://localhost:3000/contacto")
+    .then((res) => res.text()) // texto JSON → objeto JS 
+    .then((data) => { resultados.innerHTML = data;}) 
+    }); 
+boton3.addEventListener("click", () => {
+    fetch("http://localhost:3000/info")
+    .then((res) => res.json()) // texto JSON → objeto JS 
+    .then((data) => { resultados.textContent = data.nombre + ' - ' + data.curso + ' - ' + data.año;}) 
+    }); 
+boton4.addEventListener("click", () => {
+    fetch("http://localhost:3000/tienda")
+    .then((res) => res.text()) // texto JSON → objeto JS 
+    .then((data) => { resultados.textContent = data;}) 
+    }); 
