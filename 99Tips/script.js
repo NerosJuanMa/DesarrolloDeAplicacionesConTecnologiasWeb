ff53a2b07abefe973e58945652dfc22388079775
Neros__JuanMa <jmmudarra@gmail.com>
2026-01-19 12:56:34 +0100

    99

--- 99Tips/script.js
+++ 99Tips/script.js
@@ -30,3 +30,29 @@ async function cargarLikes() {
 const temp = document.querySelector('template')
 const newTemp = temp.content.cloneNode(true);
 document.body.appendChild(newTemp);
+
+//Muestra el valor del rango 
+const range = document.getElementById('miRango');
+
+// Actualizar el title cuando cambie el valor
+range.addEventListener('input', function() {
+  this.title = `Valor: ${this.value}`;
+});
+
+//Muestra el valor de Rango2
+const range2 = document.getElementById('miRango2');
+const valorDisplay = document.getElementById('valorRango');
+
+range2.addEventListener('input', function() {
+  valorDisplay.textContent = this.value;
+  this.title = `Valor: ${this.value}`;
+});
+
+//Muestra el valor de Rango3
+const range3 = document.getElementById('miRango3');
+const output = document.getElementById('rangeOutput');
+
+range3.addEventListener('input', function() {
+  output.value = this.value;
+  this.title = `Cantidad: ${this.value}`;
+});
